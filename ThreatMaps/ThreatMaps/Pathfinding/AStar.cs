61d8e1404741b8314e7c0defb5d19b9f5c7efc90
Christoph Kümper <ckuemper@gmx.de>
2016-06-01 12:50:28 +0200

    added Astar class created heuristic function

---
+++ ThreatMaps/ThreatMaps/Pathfinding/AStar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ThreatMaps.Pathfinding
+{
+
+   
+
+    class AStar
+    {
+       
+        private int heuristic(Point node, Point goal)
+        {
+            int dx = Math.Abs(node.X - goal.X);
+            int dy = Math.Abs(node.Y - goal.Y);
+            return 1 * (dx + dy) + (1 - 2 * 1) * Math.Min(dx, dy);
+        }
+
+    }
+}
